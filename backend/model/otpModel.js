c247832e684afe0cf4b5cbf9d9ac83736d7f810e
Ujjwols <shrestha.ujjwol1@gmail.com>
2025-07-29 14:07:12 +0545

    otp model

--- backend/model/otpModel.js
+++ backend/model/otpModel.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+  const mongoose = require('mongoose');
 
-const otpSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
+  const otpSchema = new mongoose.Schema(
+    {
+      email: {
+        type: String,
+        required: true,
+      },
+      token: {
+        type: String,
+        required: true,
+      },
+      expiry: {
+        type: Date,
+        required: true,
+      },
     },
-    token: {
-      type: String,
-      required: true,
-    },
-    expiry: {
-      type: Date,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+  );
 
-// Auto-delete expired records
-otpSchema.index({ expiry: 1 }, { expireAfterSeconds: 0 });
+  // Auto-delete expired records
+  otpSchema.index({ expiry: 1 }, { expireAfterSeconds: 0 });
 
-module.exports = mongoose.model('Otp', otpSchema);
+  module.exports = mongoose.model('Otp', otpSchema);
